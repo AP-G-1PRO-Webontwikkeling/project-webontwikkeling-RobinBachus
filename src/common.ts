1be137a2830e317be4c523fea059de1d9ca31e7b
RobinBachus <robinbachus@gmail.com>
2024-04-15 01:57:16 +0000

    Trying to get sorting to work

--- src/common.ts
+++ src/common.ts
@@ -10,3 +10,81 @@ export async function fetchJson(file: string) {
     );
     return data.json();
 }
+
+export type ValueType = "string" | "number" | "date";
+
+/**
+ * Compare two strings ignoring case and whitespace
+ * @param a - first string
+ * @param b - second string
+ * @returns true if strings are equal, false otherwise
+ */
+export function compareString(a: string, b: string) {
+    return a.trim().toLowerCase() === b.trim().toLowerCase();
+}
+
+/**
+ * Check if a string includes another string ignoring case and whitespace
+ * @param a - string to search in
+ * @param b - string to search for
+ * @returns true if a includes b, false otherwise
+ */
+export function includesString(a: string, b: string) {
+    return a.toLowerCase().includes(b.trim().toLowerCase());
+}
+
+/**
+ * Sort an array by a key without mutating the original array
+ * @template T - type of the array
+ * @param data - data to sort
+ * @param key - key to sort by
+ * @param order - order to sort in
+ * @returns a sorted array
+ */
+export function sortCopy<T>(
+    data: T[],
+    key: keyof T,
+    order: string,
+    valueType: ValueType
+) {
+    const copy = [...data];
+    switch (valueType) {
+        case "string":
+            return sortCopyByString(copy, key, order);
+        case "number":
+            return sortCopyByNumber(copy, key, order);
+        case "date":
+            return sortCopyByDate(copy, key, order);
+        default:
+            return copy;
+    }
+}
+
+function sortCopyByString<T>(data: T[], key: keyof T, order: string) {
+    const copy = [...data];
+    return copy.sort((a, b) => {
+        return (
+            (a[key] as string).localeCompare(b[key] as string) *
+            (order === "asc" ? 1 : -1)
+        );
+    });
+}
+
+function sortCopyByNumber<T>(data: T[], key: keyof T, order: string) {
+    const copy = [...data];
+    return copy.sort((a, b) => {
+        return (
+            (a[key] as number) - (b[key] as number) * (order === "asc" ? 1 : -1)
+        );
+    });
+}
+
+function sortCopyByDate<T>(data: T[], key: keyof T, order: string) {
+    const copy = [...data];
+    return copy.sort((a, b) => {
+        return (
+            new Date(a[key] as string).getTime() -
+            new Date(b[key] as string).getTime() * (order === "asc" ? 1 : -1)
+        );
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import ejs, { render } from "ejs";
+import ejs, { render } from "ejs"; // This is used internally by express (I think)
 
-import { fetchJson } from "./common";
+import {
+    ValueType,
+    compareString,
+    fetchJson,
+    includesString,
+    sortCopy,
+} from "./common";
 import type { Formula, Mathematician } from "./types";
 
 let data: Mathematician[] = [];
@@ -9,6 +15,8 @@ let data: Mathematician[] = [];
 (async () => {
     data = await fetchJson("mathematicians");
 })();
+
+// =================== Server ===================
 
 const app = express();
 
@@ -22,17 +30,31 @@ app.use((req, res, next) => {
     res.render("search", { results, query: req.query.search });
 });
 
+// =================== Routes ===================
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.get("/people", async (req, res) => {
+    let people = [...data];
+    let sort = "name";
+    let order = "asc";
     if (req.query.filter) {
         const filter = req.query.filter as string;
-        data = data.filter((person) => includesString(person.name, filter));
+        people = data.filter((person) => includesString(person.name, filter));
+    }
+    if (req.query.sort) {
+        sort = req.query.sort as string;
+        order = req.query.order as string;
+        people = sortData(sort as keyof Mathematician, order);
     }
 
-    res.render("people", { cards: data });
+    res.render("people", {
+        cards: people,
+        sort,
+        asc: order === "asc" ? true : false,
+    });
 });
 
 app.get("/people/:name", (req, res) => {
@@ -45,14 +67,12 @@ app.get("/people/:name", (req, res) => {
             message: "Person not found",
             status: `404 - Page Not Found`,
         });
-    else 
-    res.render("person", { person });
+    else res.render("person", { person });
 });
 
 app.get("/formulas", (req, res) => {
-    res.render("formulas", {
-        formulas: data.map((data) => data.best_known_for),
-    });
+    let formulas = data.map((data) => data.best_known_for);
+    res.render("formulas", { formulas });
 });
 
 app.get("/formulas/:name", (req, res) => {
@@ -73,25 +93,7 @@ app.listen(app.get("port"), () => {
     console.log(`Server is running on http://localhost:${app.get("port")}`);
 });
 
-/**
- * Compare two strings ignoring case and whitespace
- * @param a - first string
- * @param b - second string
- * @returns true if strings are equal, false otherwise
- */
-function compareString(a: string, b: string) {
-    return a.trim().toLowerCase() === b.trim().toLowerCase();
-}
-
-/**
- * Check if a string includes another string ignoring case and whitespace
- * @param a - string to search in
- * @param b - string to search for
- * @returns true if a includes b, false otherwise
- */
-function includesString(a: string, b: string) {
-    return a.toLowerCase().includes(b.trim().toLowerCase());
-}
+// =================== Helper functions ===================
 
 /**
  * Creates a list of mathematicians and formulas that include the search query
@@ -99,27 +101,52 @@ function includesString(a: string, b: string) {
  * @returns an array of mathematicians and formulas
  */
 function search(query: string): (Mathematician | Formula)[] {
-    const results: (Mathematician | Formula)[] = data.filter(searchPredicate, {
-        query,
-    });
+    let results = data
+        .map(searchPeople, { query })
+        .filter((result) => result !== null) as (Mathematician | Formula)[];
+
+    results = results.concat(
+        data
+            .map(searchFormulas, { query })
+            .filter((result) => result !== null) as (Mathematician | Formula)[]
+    );
 
     return results;
 }
 
 /**
- * Predicate function for the search method
- * @this - object with the search query
- * @param person - mathematician to check
- * @returns the mathematician or formula if it includes the query
+ * Checks if the persons name includes the search query
+ * @param this - an object containing the search query
+ * @param person - person to check
+ * @returns the person if found, null otherwise
  */
-function searchPredicate(
-    this: any,
-    person: Mathematician
-): Mathematician | Formula | void {
-    let query = this.query;
-    if (includesString(person.name, query)) return person;
-    if (includesString(person.best_known_for.name, query))
-        return person.best_known_for;
+function searchPeople(this: any, person: Mathematician): Mathematician | null {
+    return includesString(person.name, this.query) ? person : null;
+}
+
+/**
+ * Checks if the formula name includes the search query
+ * @param this - an object containing the search query
+ * @param formula - formula to check
+ * @returns the formula if found, null otherwise
+ */
+function searchFormulas(this: any, person: Mathematician): Formula | null {
+    return includesString(person.best_known_for.name, this.query)
+        ? person.best_known_for
+        : null;
+}
+
+/** Sorts the data by a key without mutating the original array
+ * @param key - key to sort by
+ * @param order - order to sort in
+ * @returns a sorted array
+ **/
+function sortData(key: keyof Mathematician, order: string) {
+    let type: ValueType = "string";
+    if (key === "age") type = "number";
+    if (key === "birth_date") type = "date";
+
+    return sortCopy(data, key, order, type);
 }
 
 export {};
